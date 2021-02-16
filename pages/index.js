62d24d85fdc7ba6cd02f50be92fbf561defcd6a4
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-02-16 21:37:25 +1100

    clientside key

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,19 @@ import { useState } from "react";
 import useSWR from "swr";
 import { fetcher } from "../utils";
 
-export default function Home(test) {
+export default function Home() {
   const [pageIndex, setPageIndex] = useState(0);
   const { data } = useSWR(`/posts?page=${pageIndex}`, fetcher);
   return (
     <div>
       <h1>Hello</h1>
       {data?.posts.map((post) => (
-        <p key={post.id}>{post.title}</p>
+        <>
+          <a href={`/${post.slug}`} key={post.id}>
+            {post.title}
+          </a>
+          <br />
+        </>
       ))}
     </div>
   );
--- utils/index.jsx
+++ utils/index.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://ghost.sebasptsch.dev/ghost/api/v3/content";
 axios.defaults.params = {
-  key: process.env.KEY,
+  key: "d9b31df3bebf8277d200f38dd2",
 };
 export const fetcher = (...args) => axios(...args).then((res) => res.data);
