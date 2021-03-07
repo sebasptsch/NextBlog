1a45b74bfc37bc755d6ecd6eb993046ef8864366
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-03-08 10:00:53 +1100

    add github instead of google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,9 +3,9 @@ import Providers from "next-auth/providers";
 
 const options = {
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    Providers.GitHub({
+      clientId: process.env.GTHUB_CLIENT,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   database: process.env.NEXT_PUBLIC_DATABASE_URL,
