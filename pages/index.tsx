86c43b1731628d006830a5aa7324fcad23091994
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-03-10 09:16:53 +1100

    change caption

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import {
   Box,
-  Center, Divider, Heading, HStack, IconButton, Image as ChakraImage, Stack, Text
+  Center,
+  Divider,
+  Heading,
+  HStack,
+  IconButton,
+  Image as ChakraImage,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 import React from "react";
-import { FaGithub, FaTwitter } from 'react-icons/fa';
+import { FaGithub, FaTwitter } from "react-icons/fa";
 import Layout from "../components/Layout";
 import { NextChakraLink } from "../components/NextChakraLink";
 
@@ -14,30 +21,33 @@ export default function Home() {
       <NextSeo title={`Seb's Blog`} />
       <Stack divider={<Divider />}>
         <Center>
-          <Box
-            overflow="hidden"
-            borderRadius="full"
-            w={300}
-            h={300}
-          >
+          <Box overflow="hidden" borderRadius="full" w={300} h={300}>
             <ChakraImage src="/avatar.jpg" layout="fill" objectFit="contain" />
           </Box>
         </Center>
         <Box textAlign="center">
           <Heading as="h1" mt={2} mb={2}>
             Sebastian Pietschner
-        </Heading>
+          </Heading>
           <Text as="h2">
-            A blog and personal accounting of my personal projects and
-            experiences.
-        </Text>
+            An index of my projects and experiences in the wider world.
+          </Text>
         </Box>
         <HStack spacing={10} justify="center" m={5}>
-          <IconButton aria-label="twitter" icon={<FaTwitter />} as={NextChakraLink} href={"https://twitter.com/sebasptsch"} />
-          <IconButton aria-label="github" icon={<FaGithub />} as={NextChakraLink} href={"https://github.com/sebasptsch/"} />
+          <IconButton
+            aria-label="twitter"
+            icon={<FaTwitter />}
+            as={NextChakraLink}
+            href={"https://twitter.com/sebasptsch"}
+          />
+          <IconButton
+            aria-label="github"
+            icon={<FaGithub />}
+            as={NextChakraLink}
+            href={"https://github.com/sebasptsch/"}
+          />
         </HStack>
       </Stack>
-
     </Layout>
   );
 }
