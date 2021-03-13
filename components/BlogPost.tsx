259f5189c6f700fa2541745c6fd073513db024e3
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-03-13 13:02:01 +1100

    I like images

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,9 +1,10 @@
 import { Box, Text } from "@chakra-ui/layout";
 import { Heading } from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
 import { NextChakraLinkBox } from "./NextChakraLink";
 
-export default function BlogPost({ title, summary, slug }): JSX.Element {
+export default function BlogPost({ title, summary, slug, image }): JSX.Element {
   return (
     <NextChakraLinkBox
       borderWidth="1px"
@@ -12,6 +13,18 @@ export default function BlogPost({ title, summary, slug }): JSX.Element {
       w="100%"
       href={`/posts/${slug}`}
     >
+      {image ? (
+        <Box
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "20em",
+          }}
+          borderRadius="10px"
+        >
+          <Image src={image} layout="fill" objectFit="cover" />
+        </Box>
+      ) : null}
       <Box p={4}>
         <Heading size="md">{title}</Heading>
         <Text>{summary}</Text>
--- components/ProjectPost.tsx
+++ components/ProjectPost.tsx
@@ -1,9 +1,15 @@
 import { Box, Text } from "@chakra-ui/layout";
 import { Heading } from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
 import { NextChakraLinkBox } from "./NextChakraLink";
 
-export default function ProjectPost({ title, summary, slug }): JSX.Element {
+export default function ProjectPost({
+  title,
+  summary,
+  slug,
+  image,
+}): JSX.Element {
   return (
     <NextChakraLinkBox
       borderWidth="1px"
@@ -12,6 +18,17 @@ export default function ProjectPost({ title, summary, slug }): JSX.Element {
       w="100%"
       href={`/projects/${slug}`}
     >
+      {image ? (
+        <Box
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "20em",
+          }}
+        >
+          <Image src={image} layout="fill" objectFit="cover" />
+        </Box>
+      ) : null}
       <Box p={4}>
         <Heading size="md">{title}</Heading>
         <Text>{summary}</Text>
