3b0992c0d7e5dfc1d2f9543920d18007f7d5de82
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-03-19 14:21:09 +1100

    add social profile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,15 @@
 import { Container, Flex } from "@chakra-ui/react";
+import { SocialProfileJsonLd } from "next-seo";
 import Nav from "./Nav";
 export default function Layout(props) {
   return (
     <Flex flexDir="column" h="100vh">
+      <SocialProfileJsonLd
+        type="Person"
+        name="Sebastian Pietschner"
+        url="https://sebasptsch.dev"
+        sameAs={["https://twitter.com/sebasptsch"]}
+      />
       <Nav />
 
       <Container maxW="container.md" as="main">
