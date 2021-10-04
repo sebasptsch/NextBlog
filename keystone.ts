5a810a33bc0175f78a7dd63527a971021bb3bc6a
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-10-04 13:58:19 +1100

    keystone testing

---
+++ keystone.ts
@@ -0,0 +1,33 @@
+// keystone.ts
+
+import { config, list } from "@keystone-next/keystone";
+import { text, image, timestamp } from "@keystone-next/keystone/fields";
+
+import { document } from "@keystone-next/fields-document";
+
+const Post = list({
+  fields: {
+    title: text({ isRequired: true }),
+    slug: text({ isIndexed: "unique", isFilterable: true }),
+    summary: text({ isRequired: true }),
+    image: image({ isRequired: false }),
+    published_at: timestamp({ isOrderable: true, isRequired: true }),
+    content: document({ formatting: true }),
+  },
+});
+
+export default config({
+  db: { provider: "sqlite", url: "file:./app.db" },
+  experimental: {
+    generateNextGraphqlAPI: true,
+    generateNodeAPI: true,
+  },
+  lists: { Post },
+  images: {
+    upload: "local",
+    local: {
+      storagePath: "public/images",
+      baseUrl: "/images",
+    },
+  },
+});
