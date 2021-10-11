ff770cc9aca67ff5af841d564757fc61223a93aa
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-10-11 19:04:01 +1100

    add auto slug generation

--- keystone.ts
+++ keystone.ts
@@ -5,11 +5,25 @@ import { text, image, timestamp } from "@keystone-next/keystone/fields";
 
 import { document } from "@keystone-next/fields-document";
 import { componentBlocks } from "./utils/componentBlocks";
+import _ from "lodash";
 
 const Post = list({
   fields: {
-    title: text({ isRequired: true }),
-    slug: text({ isIndexed: "unique", isFilterable: true }),
+    title: text({
+      isRequired: true,
+    }),
+    slug: text({
+      isIndexed: "unique",
+      isFilterable: true,
+      ui: {
+        createView: {
+          fieldMode: "hidden",
+        },
+        itemView: {
+          fieldMode: "hidden",
+        },
+      },
+    }),
     summary: text({ isRequired: true }),
     image: image({ isRequired: false }),
     published_at: timestamp({ isOrderable: true, isRequired: true }),
@@ -22,6 +36,18 @@ const Post = list({
       },
       componentBlocks,
     }),
+  },
+  hooks: {
+    resolveInput: ({ resolvedData }) => {
+      console.log(resolvedData);
+      const { title } = resolvedData;
+      if (title) {
+        // Ensure the first letter of the title is capitalised
+        resolvedData.slug = _.kebabCase(title);
+      }
+      // We always return resolvedData from the resolveInput hook
+      return resolvedData;
+    },
   },
 });
 
