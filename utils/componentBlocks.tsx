fce83ff71e826964bd30d273d56d4402f5488376
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-10-12 14:04:02 +1100

    new post about keystone

--- utils/componentBlocks.tsx
+++ utils/componentBlocks.tsx
@@ -1,7 +1,7 @@
 import {
-	component,
-	fields,
-	NotEditable
+  component,
+  fields,
+  NotEditable
 } from "@keystone-next/fields-document/component-blocks";
 import Gist from "react-gist";
 import SyntaxHighlighter from "react-syntax-highlighter";
@@ -11,41 +11,41 @@ import textarea from "./codeField";
 // naming the export componentBlocks is important because the Admin UI
 // expects to find the components like on the componentBlocks export
 export const componentBlocks = {
-	code: component({
-		component: ({ content, language }) => {
-			return (
-				<NotEditable>
-					<SyntaxHighlighter language={language.value} style={atomOneDark}>
-						{content.value}
-					</SyntaxHighlighter>
-				</NotEditable>
-			);
-		},
-		label: "Code",
-		props: {
-			content: textarea({
-				label: "Code",
-				defaultValue: "console.log('Hello World!');",
-			}),
-			language: fields.text({ defaultValue: "javascript", label: "Language" }),
-		},
-		chromeless: false,
-	}),
-	gist: component({
-		component: ({ id }) => {
-			return <NotEditable><Gist id={id.value} /></NotEditable>
-		},
-		label: "Gist",
-		props: {
-			id: fields.text({ label: "Id", defaultValue: "" }),
-			file: fields.text({ label: "File", defaultValue: "" })
-		}
-	}),
-	// tweet: component({
-	// 	component: ({ id }) => <Tweet id={id.value} />,
-	// 	label: "Tweet",
-	// 	props: {
-	// 		id: fields.text({ label: "Id", defaultValue: "" })
-	// 	}
-	// })
+  code: component({
+    component: ({ content, language }) => {
+      return (
+        <NotEditable>
+          <SyntaxHighlighter language={language.value} style={atomOneDark}>
+            {content.value}
+          </SyntaxHighlighter>
+        </NotEditable>
+      );
+    },
+    label: "Code",
+    props: {
+      content: textarea({
+        label: "Code",
+        defaultValue: "console.log('Hello World!');",
+      }),
+      language: fields.text({ defaultValue: "javascript", label: "Language" }),
+    },
+    chromeless: false,
+  }),
+  gist: component({
+    component: ({ id }) => {
+      return <NotEditable><Gist id={id.value} /></NotEditable>
+    },
+    label: "Gist",
+    props: {
+      id: fields.text({ label: "Id", defaultValue: "" }),
+      file: fields.text({ label: "File", defaultValue: "" })
+    }
+  }),
+  // tweet: component({
+  // 	component: ({ id }) => <Tweet id={id.value} />,
+  // 	label: "Tweet",
+  // 	props: {
+  // 		id: fields.text({ label: "Id", defaultValue: "" })
+  // 	}
+  // })
 };
