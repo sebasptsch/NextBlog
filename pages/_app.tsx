a67f89cd2c926185abe4abc200d47d30aecffa7d
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2021-11-01 17:54:21 +1100

    add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { Chakra } from "@/utils/chakra";
+import { SessionProvider } from "next-auth/react";
 import 'react-static-tweets/styles.css';
 // import "./prism.css";
 
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<Chakra cookies={pageProps.cookies}>
-			<Component {...pageProps} />
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
 		</Chakra>
 	);
 }
