2cd3c88e70b7ac8a336d1ff40f9442b7fbb45a81
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2021-11-03 21:54:54 +1100

    mess arround with spacing

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,11 @@
 import logo from "@/public/logo.png";
-import { Flex, IconButton, Spacer, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Center,
+  Flex,
+  IconButton,
+  Spacer,
+  useColorMode,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import { FaMoon, FaSun } from "react-icons/fa";
 
@@ -9,16 +15,18 @@ export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Flex pl={["5%", "20%", "25%", "30%"]} pr={["5%", "20%", "25%", "30%"]}>
-      <Image src={logo} height={"4em"} />
-      <Text fontWeight="semibold">Sebastian's Blog</Text>
+      <Center>
+        <Image src={logo} height={"4em"} layout="responsive" />
+      </Center>
       <Spacer />
-      <IconButton
-        icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
-        aria-label="toggle theme"
-        m={4}
-        onClick={toggleColorMode}
-      />
-      {/* <Auth /> */}
+      <Center>
+        <IconButton
+          icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+          aria-label="toggle theme"
+          m={4}
+          onClick={toggleColorMode}
+        />
+      </Center>
     </Flex>
   );
 }
